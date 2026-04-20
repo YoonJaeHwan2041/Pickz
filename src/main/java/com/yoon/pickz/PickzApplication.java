c118e1772290cb7ea63fbd69b591818cf369829e
JAEHWAN YOON <jkl0379@naver.com>
2026-04-20 16:58:01 +0900

    first commit

---
+++ src/main/java/com/yoon/pickz/PickzApplication.java
@@ -0,0 +1,13 @@
+package com.yoon.pickz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yoon/pickz/PickzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoon.pickz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
